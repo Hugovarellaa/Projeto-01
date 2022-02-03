28b9cdce063597ea2bcb13ad2b98c9e38684d5f3
Hugo <hugovarellaa@gmail.com>
2022-02-03 20:43:38 -0300

    Criando customer do stripe e no banco de dados e evitando duplicidade

--- ignews/src/pages/api/subcribe.ts
+++ ignews/src/pages/api/subcribe.ts
@@ -15,19 +15,24 @@ type User = {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
+    //buscar informações de login
     const session = await getSession({ req });
 
+    //buscar o usuario por email no faunadb
     const user = await faunadb.query<User>(
       q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
     );
 
+    //Evitar duplicidade no bando de dados e no stripe
     let customerId = user.data.stripe_customer_id;
 
     if (!customerId) {
+      //Cria usuario no painel do stripe
       const stripeCustomer = await stripe.customers.create({
         email: session.user.email,
         //   metadata
       });
+      //Registra o id do usuario do stripe nao banco de dados do faunadb
       await faunadb.query(
         q.Update(q.Ref(q.Collection("users"), user.ref.id), {
           data: { stripe_customer_id: stripeCustomer.id },
@@ -35,7 +40,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       );
       customerId = stripeCustomer.id;
     }
-
+    //Configuração do painel de pagamento do stripe
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
       customer: customerId,
       payment_method_types: ["card"],
