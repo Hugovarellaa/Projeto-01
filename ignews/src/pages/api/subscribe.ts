12fd27a95f8589a94161a490b1f6a7ef6cf61318
Hugo <hugovarellaa@gmail.com>
2022-02-04 16:24:22 -0300

    Incluindo no bando de dados o Id do usuario do stripe

--- ignews/src/pages/api/subscribe.ts
+++ ignews/src/pages/api/subscribe.ts
@@ -1,15 +1,38 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 import { stripe } from "../../services/stripe";
+import { query as q } from "faunadb";
+import { fauna } from "../../services/faunadb";
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_id: string;
+  };
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const session = await getSession({ req });
 
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
+
     const stripeCustomer = await stripe.customers.create({
       email: session.user.email,
       // metadata
     });
+
+    await fauna.query(
+      q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+        data: {
+          stripe_id: stripeCustomer.id,
+        },
+      })
+    );
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
       customer: stripeCustomer.id,
