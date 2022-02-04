90cb0f95e4a66023d09084db71649a9d1ede9ae0
Hugo <hugovarellaa@gmail.com>
2022-02-04 10:42:02 -0300

    Pagina home

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -6,6 +7,21 @@ export default function Home() {
       <Head>
         <title>Home | Ig.news</title>
       </Head>
+
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br /> the <span>React </span> world
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $ 9.90 month</span>
+          </p>
+          {/* botao de subscribe */}
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
