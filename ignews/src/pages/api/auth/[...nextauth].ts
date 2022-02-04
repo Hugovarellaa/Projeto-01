b62aac91d392d201ae63cc52e16a1b999f806c54
Hugo <hugovarellaa@gmail.com>
2022-02-04 15:35:42 -0300

    login social com next-auth com Github

---
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/faunadb";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_SECRET_KEY,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+      } catch (err) {
+        console.log({ err: err.message });
+      }
+      return true;
+    },
+  },
+});
