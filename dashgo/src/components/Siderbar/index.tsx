8962470a8ef7fd920503bc1f49964e55bfc734c8
hugovarellaa <hugovarellaa@gmail.com>
2022-02-14 10:57:11 -0300

    Componetizando Siderbar

--- dashgo/src/components/Siderbar/index.tsx
+++ dashgo/src/components/Siderbar/index.tsx
@@ -5,52 +5,42 @@ import {
   RiGitMergeLine,
   RiInputMethodFill,
 } from "react-icons/ri";
+import { NavSection } from "./NavSection";
 
 export function Siderbar() {
   return (
     <Box as="aside" width="64" marginRight="8">
       <Stack spacing="12" align="flex-start">
-        <Box>
-          <Text fontWeight="bold" color="gray.400" fontSize="small">
-            GERAL
-          </Text>
-          <Stack spacing="4" marginTop="8" align="stretch">
-            <Link display="flex" alignItems="center">
-              <Icon as={RiDashboardLine} fontSize="20" />
-              <Text marginLeft="4" fontWeight="medium">
-                Dashboard
-              </Text>
-            </Link>
+        <NavSection title="GERAL">
+          <Link display="flex" alignItems="center">
+            <Icon as={RiDashboardLine} fontSize="20" />
+            <Text marginLeft="4" fontWeight="medium">
+              Dashboard
+            </Text>
+          </Link>
 
-            <Link display="flex" alignItems="center">
-              <Icon as={RiContactsLine} fontSize="20" />
-              <Text marginLeft="4" fontWeight="medium">
-                Usuários
-              </Text>
-            </Link>
-          </Stack>
-        </Box>
+          <Link display="flex" alignItems="center">
+            <Icon as={RiContactsLine} fontSize="20" />
+            <Text marginLeft="4" fontWeight="medium">
+              Usuários
+            </Text>
+          </Link>
+        </NavSection>
+        <NavSection title="AUTOMAÇÃO">
+          <Link display="flex" alignItems="center">
+            <Icon as={RiInputMethodFill} fontSize="20" />
+            <Text marginLeft="4" fontWeight="medium">
+              Formulários
+            </Text>
+          </Link>
 
-        <Box>
-          <Text fontWeight="bold" color="gray.400" fontSize="small">
-            AUTOMAÇÃO
-          </Text>
-          <Stack spacing="4" marginTop="8" align="stretch">
-            <Link display="flex" alignItems="center">
-              <Icon as={RiInputMethodFill} fontSize="20" />
-              <Text marginLeft="4" fontWeight="medium">
-                Formulários
-              </Text>
-            </Link>
-
-            <Link display="flex" alignItems="center">
-              <Icon as={RiGitMergeLine} fontSize="20" />
-              <Text marginLeft="4" fontWeight="medium">
-                Automação
-              </Text>
-            </Link>
-          </Stack>
-        </Box>
+          <Link display="flex" alignItems="center">
+            <Icon as={RiGitMergeLine} fontSize="20" />
+            <Text marginLeft="4" fontWeight="medium">
+              Automação
+            </Text>
+          </Link>
+        </NavSection>
       </Stack>
     </Box>
   );
