21700645e3a4d5ebc70094aac4fb3a8b8a2f2d63
hugovarellaa <hugovarellaa@gmail.com>
2022-02-14 10:47:29 -0300

    Componentizando Header

---
+++ dashgo/src/components/Header/NotificationNav.tsx
@@ -0,0 +1,20 @@
+import { Icon, Stack } from "@chakra-ui/react";
+import { RiNotificationLine, RiUserAddLine } from "react-icons/ri";
+
+export function NotificationNav() {
+  return (
+    <Stack
+      spacing="8"
+      direction="row"
+      mx="8"
+      paddingRight="8"
+      py="1"
+      color="gray.300"
+      borderRightWidth={1}
+      borderColor="gray.700"
+    >
+      <Icon as={RiNotificationLine} fontSize="20" />
+      <Icon as={RiUserAddLine} fontSize="20" />
+    </Stack>
+  );
+}
--- dashgo/src/components/Header/index.tsx
+++ dashgo/src/components/Header/index.tsx
@@ -1,9 +1,8 @@
-import { Avatar, Box, Flex, Icon, Input, Stack, Text } from "@chakra-ui/react";
-import {
-  RiNotificationLine,
-  RiSearchLine,
-  RiUserAddLine,
-} from "react-icons/ri";
+import { Flex } from "@chakra-ui/react";
+import { Logo } from "./Logo";
+import { NotificationNav } from "./NotificationNav";
+import { Profile } from "./Profile";
+import { SearchBox } from "./SearchBox";
 
 export function Header() {
   return (
@@ -17,67 +16,11 @@ export function Header() {
       paddingX="6"
       align="center"
     >
-      {/* Logo */}
-      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" width="64">
-        DashGo
-        <Text as="span" color="pink.500" marginLeft="1">
-          .
-        </Text>
-      </Text>
-      {/* Search Box */}
-      <Flex
-        as="label"
-        flex="1"
-        paddingY="4"
-        paddingX="8"
-        marginLeft="6"
-        maxWidth={400}
-        alignSelf="center"
-        color="gray.200"
-        position="relative"
-        background="gray.800"
-        borderRadius="full"
-      >
-        <Input
-          color="gray.50"
-          variant="unstyled"
-          placeholder="Buscar na plataforma"
-          _placeholder={{
-            color: "gray.400",
-          }}
-          paddingX="4"
-          marginRight="4j"
-        />
-        <Icon as={RiSearchLine} fontSize="20" />
-      </Flex>
-      {/* Profile */}
+      <Logo />
+      <SearchBox />
       <Flex align="center" marginLeft="auto">
-        <Stack
-          spacing="8"
-          direction="row"
-          mx="8"
-          paddingRight="8"
-          py="1"
-          color="gray.300"
-          borderRightWidth={1}
-          borderColor="gray.700"
-        >
-          <Icon as={RiNotificationLine} fontSize="20" />
-          <Icon as={RiUserAddLine} fontSize="20" />
-        </Stack>
-        <Flex align="center">
-          <Box mr="4" textAlign="right">
-            <Text>Hugo Alves Varella</Text>
-            <Text color="gray.300" fontSize="small">
-              hugovarellaa@gmail.com
-            </Text>
-          </Box>
-          <Avatar
-            fontSize="md"
-            name="Hugo Varella"
-            src="https://github.com/Hugovarellaa.png"
-          />
-        </Flex>
+        <NotificationNav />
+        <Profile />
       </Flex>
     </Flex>
   );
