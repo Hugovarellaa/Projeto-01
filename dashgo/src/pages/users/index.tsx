d2bdf7304e3c1779221afeb3a0f6d748531cb119
hugovarellaa <hugovarellaa@gmail.com>
2022-02-14 09:46:37 -0300

    Criando tabela de usuários

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,4 +1,19 @@
-import { Box, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Siderbar } from "../../components/Siderbar";
 
@@ -9,6 +24,109 @@ export default function UserList() {
 
       <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Siderbar />
+
+        <Box flex="1" borderRadius={8} background="gray.800" padding="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" font-weight="normal">
+              Usuários
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr px="6">
+                <Td>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Text fontWeight="bold">Hugo Alves Varella</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    hugovarellaa@gmail.com
+                  </Text>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="18" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr px="6">
+                <Td>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Text fontWeight="bold">Hugo Alves Varella</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    hugovarellaa@gmail.com
+                  </Text>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="18" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr px="6">
+                <Td>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Text fontWeight="bold">Hugo Alves Varella</Text>
+                  <Text fontSize="sm" color="gray.300">
+                    hugovarellaa@gmail.com
+                  </Text>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="18" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
       </Flex>
     </Box>
   );
