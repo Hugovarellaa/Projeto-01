859efdb7c884329574cf85c9c048269d1c6216bf
hugovarellaa <hugovarellaa@gmail.com>
2022-02-14 10:06:18 -0300

    Pagina de criação de usuario

--- dashgo/src/pages/users/create.tsx
+++ dashgo/src/pages/users/create.tsx
@@ -1,8 +1,18 @@
-import { Box, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  Stack,
+} from "@chakra-ui/react";
+import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Siderbar } from "../../components/Siderbar";
 
-export default function UserList() {
+export default function CreateUser() {
   return (
     <Box>
       <Header />
@@ -10,7 +20,34 @@ export default function UserList() {
       <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Siderbar />
 
-        <Box flex="1" borderRadius={8} background="gray.800" padding="8"></Box>
+        <Box flex="1" borderRadius={8} background="gray.800" padding="8">
+          <Heading size="lg" fontWeight="normal">
+            Criar usuário
+          </Heading>
+          <Divider my="6" borderColor="gray.700" />
+
+          <Stack direction="column" spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
+              <Input name="name" label="Nome Completo" />
+              <Input name="email" label="E-mail" type="email" />
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
+              <Input name="password" label="Senha" type="password" />
+              <Input
+                name="password_confirmation"
+                label="Confirme sua senha"
+                type="password"
+              />
+            </SimpleGrid>
+          </Stack>
+          <Flex marginTop="8" justify="flex-end">
+            <HStack spacing="4">
+              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Button colorScheme="pink">Salvar</Button>
+            </HStack>
+          </Flex>
+        </Box>
       </Flex>
     </Box>
   );
