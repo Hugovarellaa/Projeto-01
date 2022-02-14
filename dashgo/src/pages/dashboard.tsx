7bf1b85f6410e443a05183498f7ff0389626d144
hugovarellaa <hugovarellaa@gmail.com>
2022-02-14 08:52:33 -0300

    menu siderbar

--- dashgo/src/pages/dashboard.tsx
+++ dashgo/src/pages/dashboard.tsx
@@ -1,9 +1,15 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Siderbar } from "../components/Siderbar";
 
 export default function Dashboard() {
   return (
-    <>
+    <Flex direction="column" height="100vh">
       <Header />
-    </>
+
+      <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Siderbar />
+      </Flex>
+    </Flex>
   );
 }
